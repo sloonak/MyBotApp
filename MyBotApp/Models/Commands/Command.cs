dee54c2b7b04354ee59069c4d401233d00bad8bd
Akinok <nuqpa1@mail.ru>
2021-03-03 00:27:08 +0300

    Убрал упоминание бота в методе contains

--- MyBotApp/Models/Commands/Command.cs
+++ MyBotApp/Models/Commands/Command.cs
@@ -11,7 +11,7 @@ namespace MyBotApp.Models.Commands
 
         public bool Contains(string command)
         {
-            return command.Contains(this.Name) && command.Contains(AppSettings.Name);
+            return command.Contains(this.Name);
         }
     }
 }
