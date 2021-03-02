43a0d462b36e9af82c2180f37cbae109d4da3eed
Akinok <nuqpa1@mail.ru>
2021-03-02 23:54:33 +0300

    Убрал async и await в методе Execute в HelloCommand

--- MyBotApp/Models/Commands/HelloCommand.cs
+++ MyBotApp/Models/Commands/HelloCommand.cs
@@ -10,12 +10,12 @@ namespace MyBotApp.Models.Commands
     public class HelloCommand : Command
     {
         public override string Name => "hello";
-        public override async void Execute(Message message, TelegramBotClient client)
+        public override void Execute(Message message, TelegramBotClient client)
         {
             var chatId = message.Chat.Id;
             var messageId = message.MessageId;
 
-            await client.SendTextMessageAsync(chatId, "Hello", replyToMessageId: messageId);
+            client.SendTextMessageAsync(chatId, "Hello", replyToMessageId: messageId);
         }
     }
 }
